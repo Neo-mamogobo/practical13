e62bc7631442f48e2fc62023a4b4f57f026bf726
Neo-mamogobo <dipinamamogobo@gmail.com>
2026-03-02 14:51:05 +0200

    first commit

--- openchained.java
+++ openchained.java
@@ -1,22 +1,40 @@
 public class openchained {
 
     static class openhash{
-         String[] table;
+         int[] table;
          int size;
 
          public void openchained(int size){
              this.size = size;
-             table = new String[size];
+             table = new int[size];
 
              for(int i = 0; i < size; i++){
-                 table[i] = String.valueOf(-1);
+                 table[i] = -1;
              }
          }
-         private int hash(String key){
-             int i = key % size;
-             return i;
+         private int hash(int key){
+             return key %  size;
          }
+         public void insect( int item){
+             int index = hash(item);
 
+             while(table[index] != -1) {
+                 index = (index + 1) % size;
+             }
+             table[index] = item;
+         }
+         public void delete(int item){
+             int index = hash(item);
+             int start = index;
+
+             while(table[index] != -1){
+                 if(table[index]== item){
+                     table[index] =-2;
+                     return;
+                 }
+                 if(table[index])
+             }
+         }
     }
 
 }
