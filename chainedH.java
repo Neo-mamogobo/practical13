e8779b613778895dad0c31312f7ce0ebaf8e7ace
Neo-mamogobo <dipinamamogobo@gmail.com>
2026-03-02 15:47:30 +0200

    Start hashing project

--- chainedH.java
+++ chainedH.java
@@ -4,16 +4,17 @@ public class chainedH {
         int data;
         Node next;
         private int size;
+        private Node[] table;
 
         public Node(int data){
             this.data = data;
             this.next = null;
         }
-        static class chainhash{
+        static class ChainHash{
             private Node[] table;
             private int size;
         }
-        public void chainhash(int size){
+        public void ChainHash(int size){
             this.size = size;
             table = new Node[size];
         }
@@ -25,6 +26,47 @@ public class chainedH {
             Node newNode = new Node(item);
             newNode.next = table[index];
             table[index] = newNode;
+        }
+        public boolean search(int item){
+            int index = hash(item);
+            Node current  = table[index];
+            
+            while (current != null){
+                if(current.data == item){
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
+        }
+        public void delete(int item) {
+            int index = hash(item);
+            Node current = table[index];
+            Node prev = null;
+
+            while (current != null) {
+                if (current.data == item) {
+                    if (prev.data == item) {
+                        table[index] = current.next;
+                    } else {
+                        prev.next = current.next;
+                    }
+                    return;
+                }
+                prev = current;
+                current = current.next;
+            }
+        }
+        public void display(){
+           for(int i = 0; i < size; i++){
+               System.out.println(i);
+               Node current = table[i];
+               
+               while(current != null){
+                   System.out.println(current.data + " to ");
+                   current = current.next;
+               }
+           }
         }
     }
 }
